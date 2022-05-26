2bc8438a60d38f0bbbb5637ca3ca9119a1135c38
dimuthu_l <dimuthu_l@epiclanka.net>
2022-05-26 20:28:24 +0530

    Finished 95% things in assignment properly!

--- app/src/main/java/com/ktl1/starwars/presentation/planet_list/PlanetListScreen.kt
+++ app/src/main/java/com/ktl1/starwars/presentation/planet_list/PlanetListScreen.kt
@@ -1,23 +1,20 @@
 package com.ktl1.starwars.presentation.planet_list
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Colors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.google.accompanist.flowlayout.FlowRow
 import com.ktl1.starwars.data.remote.dto.toPlanet
 import com.ktl1.starwars.presentation.Screen
 import com.ktl1.starwars.presentation.planet_list.component.PlanetListItem
@@ -46,13 +43,18 @@ fun PlanetListScreen(
                 }
             }
             state.planetList?.let {
-                items(it.results) { planet ->
+
+                println(it.results.size)
+                items(it.results.size) { planet ->
+                    println(planet)
                     PlanetListItem(
-                        planet = planet.toPlanet(),
+                        planet = it.results[planet].toPlanet(),
                         onItemClick = {
-                            navController.navigate(Screen.PlanetDetailsScreen.route + "/${25}")
+                            navController.navigate(Screen.PlanetDetailsScreen.route + "/${planet+1}")
                         }
+
                     )
+                    println("Done")
                 }
             }
         }
